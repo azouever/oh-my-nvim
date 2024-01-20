f5fe20b7412fc046a75c01543fa04dc022b802e5
azouever <azouever@gmail.com>
2024-01-20 09:37:12 +0800

    clipboard communicate with os

--- lua/azou_custom_core.lua
+++ lua/azou_custom_core.lua
@@ -1,4 +1,3 @@
-
 -- Setup keymapping
 local function set_keymap()
 	local map = vim.keymap.set
@@ -7,22 +6,26 @@ local function set_keymap()
 
 	-- 这几个配置可能不是很需要,暂时先放着
 	-- clean line below
-	map("n",";j", "m`:silent +g/\\m^\\s*$/d<CR>``:noh<CR>", option)
+	map("n", ";j", "m`:silent +g/\\m^\\s*$/d<CR>``:noh<CR>", option)
 	-- clean line above
-	map("n",";k", "m`:silent -g/\\m^\\s*$/d<CR>``:noh<CR>", option)
+	map("n", ";k", "m`:silent -g/\\m^\\s*$/d<CR>``:noh<CR>", option)
 
 	-- add blank line below
-	map("n"," j", ":set paste<CR>m`o<Esc>``:set nopaste<CR>", option)
+	map("n", " j", ":set paste<CR>m`o<Esc>``:set nopaste<CR>", option)
 	-- add blank line above
-	map("n"," k", ":set paste<CR>m`O<Esc>``:set nopaste<CR>", option)
+	map("n", " k", ":set paste<CR>m`O<Esc>``:set nopaste<CR>", option)
 
 	map("i", "jk", "<Escape>", option)
-    map("i", "kj", "<Escape>", option)
+	map("i", "kj", "<Escape>", option)
 
 	-- zen-mode
-    map("n", "<leader>z", ":ZenMode<CR>", option)
-
+	map("n", "<leader>z", ":ZenMode<CR>", option)
 end
 
+local function set_custom_opt()
+	-- set clipboard = unnamedplus
+	vim.opt.clipboard = "unnamedplus"
+end
 
 set_keymap()
+set_custom_opt()
