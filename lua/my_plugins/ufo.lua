71e09a7b29092971fe84a16f9d8aae7b9ab2b169
azouever <azouever@gmail.com>
2024-03-23 09:15:08 +0800

    feat:clean fold plugin ufo

--- lua/my_plugins/ufo.lua
+++ lua/my_plugins/ufo.lua
@@ -1,13 +1,13 @@
 local M = {
-	"kevinhwang91/nvim-ufo",
-	dependencies = { "kevinhwang91/promise-async" },
-	opts = {
-		filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
-	},
-	config = function(_, opts)
-		vim.opt.foldlevelstart = 99
-		require("ufo").setup(opts)
-	end,
+	-- "kevinhwang91/nvim-ufo",
+	-- dependencies = { "kevinhwang91/promise-async" },
+	-- opts = {
+	-- 	filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+	-- },
+	-- config = function(_, opts)
+	-- 	vim.opt.foldlevelstart = 99
+	-- 	require("ufo").setup(opts)
+	-- end,
 }
 
 return M
