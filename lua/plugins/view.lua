c988fdc33d090f8861b9cfacf8660fe95f9e2ed4
azouever <azouever@gmail.com>
2024-09-02 15:11:10 +0800

    reorganize all the configurations, this version running no error----ooookkkkk

--- lua/plugins/view.lua
+++ lua/plugins/view.lua
@@ -41,18 +41,18 @@ return {
     -- },
 
     -- File explorer
-    -- {
-    --     "nvim-neo-tree/neo-tree.nvim",
-    --     branch = "v2.x",
-    --     dependencies = {
-    --         "nvim-lua/plenary.nvim",
-    --         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    --         "MunifTanjim/nui.nvim",
-    --     },
-    --     config = function()
-    --         require("plugins/neo-tree/config")()
-    --     end,
-    -- },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v2.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+        },
+        config = function()
+            require("plugins/neo-tree/config")()
+        end,
+    },
 
     -- -- Outline
     -- {
