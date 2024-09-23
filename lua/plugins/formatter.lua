b60b9091ea76002581901437ea901f1ae950da86
azouever <azouever@gmail.com>
2024-09-24 01:04:26 +0800

    feat: toml file format use taplo default config by .taplo.toml file in home dir

---
+++ lua/plugins/formatter.lua
@@ -0,0 +1,40 @@
+return {
+  -- 其他插件...
+  {
+    "mhartington/formatter.nvim",
+    config = function()
+      require("formatter").setup({
+        filetype = {
+        --   lua = {
+        --     function()
+        --       return {
+        --         exe = "lua-format",
+        --         args = { "--indent-width=2" },
+        --         stdin = true,
+        --       }
+        --     end,
+        --   },
+        --   python = {
+        --     function()
+        --       return {
+        --         exe = "black",
+        --         args = { "-" },
+        --         stdin = true,
+        --       }
+        --     end,
+        --   },
+          -- 其他文件类型的配置...
+          -- toml = {
+          --   function()
+          --     return {
+          --       exe = "taplo",
+          --       args = { "format", "-" },
+          --       stdin = true,
+          --     }
+          --   end,
+          -- },
+        },
+      })
+    end,
+  },
+}
