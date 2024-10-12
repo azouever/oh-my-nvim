50124769efd1135d8664db91ea40c3fec81096eb
azouever <azouever@gmail.com>
2024-10-12 18:17:45 +0800

    feat:test window disappear issue

--- lua/my_plugins/copilot.lua
+++ lua/my_plugins/copilot.lua
@@ -4,13 +4,40 @@ return {
 		lazy = false,
 		config = function()
 			-- Mapping tab is already used by NvChad
-			vim.g.copilot_no_tab_map = true
+			-- vim.g.copilot_no_tab_map = true
 			-- vim.g.copilot_assume_mapped = true;
 			-- vim.g.copilot_tab_fallback = "";
-			-- The mapping is set to other key, see custom/lua/mappings
-			-- or run <leader>ch to see copilot mapping section
-			--vim.g.copilot_no_tab_map = true
-			vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+			-- vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 		end,
 	},
+
+  -- {
+  --   "CopilotC-Nvim/CopilotChat.nvim",
+  --   lazy = true,
+  --   branch = "canary",
+  --   dependencies = {
+  --     { "zbirenbaum/copilot.lua" },
+  --     { "nvim-lua/plenary.nvim" },
+  --   },
+  --   opts = {
+  --     debug = false,
+  --     window = {
+  --       width = 0.25,
+  --     },
+  --   },
+  --   keys = {
+  --     {
+  --       "<Leader>ch",
+  --       ":'<,'>CopilotChat<CR>",
+  --       mode = { "v" },
+  --       desc = "Copilot Chat Selection",
+  --     },
+  --     {
+  --       "<Leader>ch",
+  --       ":CopilotChatToggle<CR>",
+  --       mode = { "n" },
+  --       desc = "Toggle Copilot Chat",
+  --     },
+  --   },
+  -- },
 }
--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -126,14 +126,14 @@ return {
 
 	-- { "rcarriga/nvim-notify" },
 
-	-- -- resize the window
-	-- {
-	-- 	"mrjones2014/smart-splits.nvim",
-	-- 	version = "v1.2.2",
-	-- 	config = function()
-	-- 		require("plugins/smart-split/config")()
-	-- 	end,
-	-- },
+	-- resize the window
+	{
+		"mrjones2014/smart-splits.nvim",
+		version = "v1.2.2",
+		config = function()
+			require("plugins/smart-split/config")()
+		end,
+	},
 
 	-- -- buffer delete
 	-- { "famiu/bufdelete.nvim" },
